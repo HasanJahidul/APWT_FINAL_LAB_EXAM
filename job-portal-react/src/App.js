dddd8e6dc911a6ea8340f0fe32e511cd40b48e72
HasanJahidul <joyaiub1@gmail.com>
2021-08-04 14:04:22 +0600

    add job done

--- job-portal-react/src/App.js
+++ job-portal-react/src/App.js
@@ -1,9 +1,23 @@
 import RegisterEmployer from './components/employer/RegisterEmployer';
 import AddJob from './components/job/AddJob';
 import Login from './components/login';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';;
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
+  const addJob=(newJob)=>{
+    fetch('http://localhost:8000/api/jobs', {
+                          method: "Post",
+                          headers:{
+                              'Content-Type':'application/json'
+                          },
+                          body: JSON.stringify(newJob)
+                      }).then((result)=>{
+                          result.json().then((resp)=>{
+                            alert(" Job added")
+                            getJobData()
+                          })
+                      })
+  }
   return (
     <Router>
       
@@ -13,7 +27,7 @@ function App() {
           <Login />
           </Route>
           <Route path='/register'>
-              <RegisterEmployer status='add'/>
+              <RegisterEmployer status='add' addJobCallback={addJob}/>
           </Route>
 
           <Route path='/job/add'>
--- job-portal-react/src/components/job/AddJob.js
+++ job-portal-react/src/components/job/AddJob.js
@@ -1,21 +1,57 @@
-
 import EmployerNavbar from '../EmployerNavbar';
+import { useParams  } from "react-router";
+import React, { useState, useEffect } from 'react';
+import { useHistory } from "react-router-dom";
+
+const AddJob = ({status, addJobCallback, updateJobCallback})=>{
+    let history = useHistory();
+    const {id:eid} = useParams();
+    const initialState = {name: '',quantity:'', price: '' };
+    const [job, setJob] = useState(initialState);
+  
+    const handleInputChange = (event) => {
+      const { name, value } = event.target;
+  
+      setJob({ ...job, [name]: value });
+    };
 
 
-const AddJob = ()=>{
-   
+    useEffect(() => {
+      fetch('http://localhost:8000/api/job/'+eid).then((response) => {
+        response.json().then((result) => {
+            console.warn(result)
+            setJob({ 
+                quantity:result.quantity,
+                 name:result.name,
+                 price:result.price,
+                 
+
+              })
+        })
+    })
+    },[]);
     return(
         <>
         <EmployerNavbar />
             <br/>
-            <h3>Job Page</h3>
+            <h3>{status==='add'?'Add':'Edit'} job Page: {eid}</h3>
             <form
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    status=='add'?addJobCallback(job):updateJobCallback(eid,job);
+                    history.push("/job/list");
+                }}
             >
-                Company Name: <input type='text' name='name' /> <br/>
-                Job Title: <input type='text' name='title' /><br/>
-                Job Location : <input type='text' name='location' /><br/>
-                Salary : <input type='text' name='salary' /><br/>
-                <input type='submit' />
+                <th>
+                <strong>Company Name:</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <input type='text' name='name' value={job.name} onChange={handleInputChange}/> <br/>
+                <strong>Job Title: </strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type='text' name='title' value={job.title} onChange={handleInputChange} /><br/>
+                <strong>Job Location: </strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type='text' name='location' value={job.location} onChange={handleInputChange} /><br/>
+                <strong>Salary: </strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                <input type='text' name='salary' value={job.salary} onChange={handleInputChange} /><br/>
+                <input class="btn btn-fill btn-primary" type='submit' value={status==='add'?'Add':'Update'}/>
+                </th>
             </form>
         </>
     );
